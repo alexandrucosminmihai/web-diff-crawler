3fee95fa53b6842772b1115ac995bb58f1a3132a
Alex Mihai <alexcosmin.mihai@gmail.com>
2019-03-14 21:18:21 +0200

    Remove matchingrule column from notifications table And keep only the id_matchingrule foreign key

--- webDiffCrawler/webDiffCrawler/mappedClasses.py
+++ webDiffCrawler/webDiffCrawler/mappedClasses.py
@@ -18,19 +18,22 @@ class Crawlingrules(Base):
                "contributor='%s', content='%s')>" % (self.id_crawlingrules, self.address, self.selectionrule,
                                                      self.lastmodifytime, self.contributor, self.content)
 
-# id_notifications | address | matchingrule | modifytime | content | recipients | ackers
+# id_notifications | address | id_matchingrule | modifytime | oldcontent | changes | recipients | ackers
 class Notifications(Base):
     __tablename__ = 'notifications'
     id_notifications = Column(Integer, Sequence('notifications_id_notifications_seq'), primary_key=True)
-    address = Column(String)
-    matchingrule = Column(String)
+    address = Column(String, nullable=False)
+    # matchingrule = Column(String, nullable=False)
+    id_matchingrule = Column(Integer)
     modifytime = Column(TIMESTAMP(True))
-    content = Column(String)
+    currcontent = Column(String)
+    oldcontent = Column(String)
+    changes = Column(String, nullable=False)
     recipients = Column(ARRAY(String))
     ackers = Column(ARRAY(String)) # It's an array in case of future use. Normally should have maximum 1 element
 
     def __repr__(self):
-        return "<Notification(id_notifications='%s', address='%s', matchingrule='%s', modifytime='%s', " \
-               "content='%s', recipients='%s', ackers='%s')>" % (self.id_notifications, self.address,
-                                                                 self.matchingrule, self.modifytime, self.content,
-                                                                 self.recipients, self.ackers)
+        return "<Notification(id_notifications='%s', address='%s', matchingrule='%s', id_matchingrule='%s'," \
+               " modifytime='%s', oldcontent='%s', changes='%s' recipients='%s', ackers='%s')>" % \
+               (self.id_notifications, self.address, self.matchingrule, self.id_matchingrule, self.modifytime,
+                self.oldcontent, self.changes, self.recipients, self.ackers)
