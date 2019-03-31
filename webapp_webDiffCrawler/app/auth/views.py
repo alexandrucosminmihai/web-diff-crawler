713fba5c2d19695fa35b9c43d6578bab4b8afa48
Alex Mihai <alexcosmin.mihai@gmail.com>
2019-03-31 12:40:22 +0300

    Use the new models module for the webapp

--- webapp_webDiffCrawler/app/auth/views.py
+++ webapp_webDiffCrawler/app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from webapp_webDiffCrawler.app.main import main
-from webDiffCrawler.webDiffCrawler.mappedClasses import Users
+from webapp_webDiffCrawler.app.models import Users
 from webapp_webDiffCrawler.app import dbSession
 from .forms import LoginForm, RegistrationForm, SecretTokenGenrationForm
 
--- webapp_webDiffCrawler/webapp.py
+++ webapp_webDiffCrawler/webapp.py
@@ -2,5 +2,3 @@ import os
 from webapp_webDiffCrawler.app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-# import webDiffCrawler.webDiffCrawler.mappedClasses as mappedClasses
